873b9919ff2298095e33e3ffe3a8ca33c440d803
sahebjohar92 <sahebsingh10@gmail.com>
2019-03-26 23:46:43 -0700

    Adding gdb thread class and update UI function

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import sys
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QPainter, QPen
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtWidgets import QWidget,QLabel, QSplitter,QApplication, QHBoxLayout, QStackedWidget,QFrame,QVBoxLayout,QScrollArea,QListWidget,  QMessageBox,QTableWidget,QTableWidgetItem
 from PyQt5.QtGui import QPainter, QPen , QPixmap
 from PyQt5.QtCore import Qt
+from pysimulavrExample.examples import SimulavrAdaptor
 
 import Components.stackedWidget
 import Components.Register_Values
@@ -20,6 +21,14 @@ class Landing(QtWidgets.QWidget):
 	def __init__(self):
 	    super(Landing, self).__init__()
 	    self.initUI()
+
+		self.Register_Values = None
+		self.List_of_Registers = None
+
+		self.PIN_Diagram = None
+
+		self.stackWidget = None
+		self.initUI()
 	    
 	    
 	def initUI(self):
@@ -38,61 +47,61 @@ class Landing(QtWidgets.QWidget):
 
 	def window(self):
 
-		Register_Values = Components.Register_Values.Register_Values().getInstance()  # Object of Class Register Values
-		List_of_Registers = Components.List_of_Registers.List_of_Registers().getInstance() # Object of Class List of Registers
+		self.Register_Values = Components.Register_Values.Register_Values().getInstance()  # Object of Class Register Values
+		self.List_of_Registers = Components.List_of_Registers.List_of_Registers().getInstance() # Object of Class List of Registers
 
-		PIN_Diagram = Components.ATMega_PIN_Diagram.PIN_Diagram() # Object of Class PIN Diagram
+		self.PIN_Diagram = Components.ATMega_PIN_Diagram.PIN_Diagram() # Object of Class PIN Diagram
 
-		stackWidget = Components.stackedWidget.stackWidget().getInstance();
+		self.stackWidget = Components.stackedWidget.stackWidget().getInstance();
 
 
 
-		splitter = QSplitter(Qt.Vertical);
+		splitter = QSplitter(Qt.Vertical)
 
-		splitter.addWidget(List_of_Registers)
-		splitter.addWidget(Register_Values)
+		splitter.addWidget(self.List_of_Registers)
+		splitter.addWidget(self.Register_Values)
 		splitter.setSizes([300,150])
 
-		horizontalLayout = QHBoxLayout()
+		self.horizontalLayout = QHBoxLayout()
 
-		rightFrame = PIN_Diagram.getPIN_Digram();
+		self.rightFrame = PIN_Diagram.getPIN_Digram()
 
-		stackWidget.addWidget(rightFrame);
-		print(stackWidget.currentWidget())
+		self.stackWidget.addWidget(self.rightFrame)
+		print(self.stackWidget.currentWidget())
 
-		horizontalSplitter = QSplitter(Qt.Horizontal)
-		horizontalLayout.addWidget(splitter)
-		horizontalSplitter.addWidget(stackWidget)
-		horizontalSplitter.setSizes([80,320])
-		horizontalSplitter.adjustSize()
+		self.horizontalSplitter = QSplitter(Qt.Horizontal)
+		self.horizontalLayout.addWidget(splitter)
+		self.horizontalSplitter.addWidget(stackWidget)
+		self.horizontalSplitter.setSizes([80,320])
+		self.horizontalSplitter.adjustSize()
 
-		titleFont =  QtGui.QFont("Arial", 15, QtGui.QFont.Bold)
-		Title = QtWidgets.QLabel(self)
-		Title.setText("ATMega328p Simulator")
-		Title.setFont(titleFont)
-		Title.setAlignment(Qt.AlignCenter)
+		self.titleFont =  QtGui.QFont("Arial", 15, QtGui.QFont.Bold)
+		self.Title = QtWidgets.QLabel(self)
+		self.Title.setText("ATMega328p Simulator")
+		self.Title.setFont(titleFont)
+		self.Title.setAlignment(Qt.AlignCenter)
 
-		backButton = QtWidgets.QPushButton("Back")
-		backButton.clicked.connect(lambda : self.backClicked())
-
-
-		horizontalLayout.addWidget(horizontalSplitter)
+		self.backButton = QtWidgets.QPushButton("Back")
+		self.backButton.clicked.connect(lambda : self.backClicked())
 
 
-		StatusFont =  QtGui.QFont("Helvetica", 10, QtGui.QFont.Bold)
-		Status = QtWidgets.QLabel(self)
-		Status.setText(self.getConnectionStatus())
-		Status.setFont(StatusFont)
-		Status.setStyleSheet('color : green')
-		Status.setAlignment(Qt.AlignCenter)
+		self.horizontalLayout.addWidget(horizontalSplitter)
 
-		verticalLayout = QVBoxLayout()
-		verticalLayout.addWidget(Title)
-		verticalLayout.addWidget(Status)
-		verticalLayout.addWidget(backButton, 0, Qt.AlignRight)
-		verticalLayout.addLayout(horizontalLayout);
 
-		self.setLayout(verticalLayout)
+		self.StatusFont =  QtGui.QFont("Helvetica", 10, QtGui.QFont.Bold)
+		self.Status = QtWidgets.QLabel(self)
+		self.Status.setText(self.getConnectionStatus())
+		self.Status.setFont(self.StatusFont)
+		self.Status.setStyleSheet('color : green')
+		self.Status.setAlignment(Qt.AlignCenter)
+
+		self.verticalLayout = QVBoxLayout()
+		self.verticalLayout.addWidget(self.Title)
+		self.verticalLayout.addWidget(self.Status)
+		self.verticalLayout.addWidget(self.backButton, 0, Qt.AlignRight)
+		self.verticalLayout.addLayout(self.horizontalLayout);
+
+		self.setLayout(self.verticalLayout)
 
 	def getConnectionStatus(self):  # Function returns status (Connected / Disconnected)
 		return "Connected to Simulavr"
@@ -105,11 +114,29 @@ class Landing(QtWidgets.QWidget):
 			Components.stackedWidget.stackWidget.decrementTopCount()
 			Components.stackedWidget.stackWidget.removeWidget(widgetToRemove)
 
+	def updateUI(self, valueMap):
 
+		self.valueMap = valueMap
+
+		for key, value in valueMap.items():
+
+			if key == 'PORTB':
+				self.PIN_Diagram.setPinStatus("PD0", value)
+
+class threadExample(QThread):
+	def __init__(self, ui, sim):
+		QThread.__init__(self)
+		self.sim = sim
+		self.ui = ui
+		self.start()
+
+	def run(self):
+		self.sim.runProgram(self.ui)
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    ex = Landing()
-    sys.exit(app.exec_())
-
+	obj = Landing()
+	sim = SimulavrAdaptor.SimulavrAdapter()
+	thread = threadExample(obj, sim)
+	app.exec_()
