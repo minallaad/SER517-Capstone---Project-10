14f614e98653373db3f567630b46a84981d5f665
Ayan Shah <arshah12@asu.edu>
2019-04-11 01:13:28 -0700

    changes

--- simulavr/adaptor/SimulavrAdaptor.py
+++ simulavr/adaptor/SimulavrAdaptor.py
@@ -4,6 +4,7 @@ import Components.EEPROM
 
 class SimulavrAdapter(object):
     DEFAULT_CLOCK_SETTING = 63
+    uiUpdateFlag = False
 
     def loadDevice(self, t, e):
         self.__sc = pysimulavr.SystemClock.Instance()
@@ -22,10 +23,14 @@ class SimulavrAdapter(object):
 
         i = 0
         while True:
-
+            print('loop')
             if i == 5000:
                 self.getMemoryValue(dev)
-                ui.updateUI()
+
+                if self.uiUpdateFlag == True :
+                    ui.updateUI()
+
+                    self.uiUpdateFlag == False
 
                 if Components.Globalmap.Map.refresh_flag:
 
@@ -60,6 +65,9 @@ class SimulavrAdapter(object):
     def getDDRValues(self, dev):
         for key, value in Components.Globalmap.Map.registerAddressMap.items():
             val = dev.getRWMem(value)
+
+            if (len(Components.Globalmap.Map.map) == len(Components.Globalmap.Map.registerAddressMap) and val != Components.Globalmap.Map.map[key]) :
+                self.uiUpdateFlag = True
             Components.Globalmap.Map.map[key] = val
 
     def getMemoryDumpRange(self, dev):
