5722474ae700203f87e725a842404531bdd72450
Minal <minal.laad@gmail.com>
2019-04-18 23:31:29 -0700

    Added title to 16 Bit Timer Screen

--- Components/Timer_16Bit.py
+++ Components/Timer_16Bit.py
@@ -17,15 +17,24 @@ class Ui_Frame(object):
         Frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         Frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.label = QtWidgets.QLabel(Frame)
-        self.label.setGeometry(QtCore.QRect(0, -30, 831, 711))
+        self.label.setGeometry(QtCore.QRect(240, 30, 301, 31))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
         self.label.setObjectName("label")
 
+        self.label_2 = QtWidgets.QLabel(Frame)
+        self.label_2.setGeometry(QtCore.QRect(90, 90, 671, 501))
+        self.label_2.setObjectName("label_2")
         self.retranslateUi(Frame)
         QtCore.QMetaObject.connectSlotsByName(Frame)
 
     def retranslateUi(self, Frame):
         _translate = QtCore.QCoreApplication.translate
         Frame.setWindowTitle(_translate("Frame", "Frame"))
-        self.label.setText(_translate("Frame", "<html><head/><body><p><img src=\":/image/Images/16 bit counter.png\"/></p></body></html>"))
+        self.label.setText(_translate("Frame", "16 Bit Timer"))
+        self.label_2.setText(_translate("Frame", "<html><head/><body><p><img src=\":/image/Images/16 bit counter.png\"/></p></body></html>"))
 
 
