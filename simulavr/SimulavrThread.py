7a04a761cbc7b59f4825e751db921be7b182121a
aman25m <amanmaheshwari25@gmail.com>
2019-04-20 21:43:51 -0700

    adding description for SimulavrThread.py

--- simulavr/SimulavrThread.py
+++ simulavr/SimulavrThread.py
@@ -1,3 +1,7 @@
+'''
+Description: This class is used to create a thread for simulavr for running it seperately from main thread.
+			 The run function of this thread invokes a method call which is reponsible for running of the program.
+'''
 from PyQt5.QtCore import QThread
 
 class simulavrThread(QThread):
