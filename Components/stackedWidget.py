7e1d5da8b2063f289d69cbb61053561d1f0ff874
aman25m <amanmaheshwari25@gmail.com>
2019-04-23 19:32:50 -0700

    adding detailed comments in stacked widget class

--- Components/stackedWidget.py
+++ Components/stackedWidget.py
@@ -22,17 +22,22 @@ class stackWidget(QtWidgets.QWidget):
             stackWidget.StackWidget = QStackedWidget()
             stackWidget.top = 0
 
-    """ Static Method to add the Object of QFrame on Stack  """
+    """ 
+    Description: Static Method to add the Object of QFrame on Stack  
+    @param qFrame: Frame of a component that needs to be added."""
     @staticmethod
     def addWidget(qFrame):
         stackWidget.StackWidget.addWidget(qFrame)
 
-    """ Static Method to remove the Object of QFrame on Stack  """
+    """ 
+    Description: Static Method to remove the Object of QFrame on Stack  
+    @param qFrame: Frame of a component that needs to be added.
+    """
     @staticmethod
     def removeWidget(qFrame):
         stackWidget.StackWidget.removeWidget(qFrame)
 
-    """ Static Method to remove the Object of QFrame on Stack  """
+
     @staticmethod
     def getInstance():
         """ Static access method. """
@@ -40,16 +45,20 @@ class stackWidget(QtWidgets.QWidget):
             stackWidget()
         return stackWidget.StackWidget
 
-    """ Static Method to increment the top counter   """
+    """ 
+    Description: Static Method to increment the top counter.   
+    """
     @staticmethod
     def incrementTopCount():
-        stackWidget.top = stackWidget.top + 1;
+        stackWidget.top = stackWidget.top + 1
         stackWidget.StackWidget.setCurrentIndex(stackWidget.top)
 
-    """ Static Method to decrement the top counter   """
+    """ 
+    Description: Static Method to decrement the top counter.
+    """
     @staticmethod
     def decrementTopCount():
-        stackWidget.top = stackWidget.top - 1;
+        stackWidget.top = stackWidget.top - 1
         stackWidget.StackWidget.setCurrentIndex(stackWidget.top)
 
 
