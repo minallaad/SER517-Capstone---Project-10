f7a48f1a284c55a660eb0fb92d47a7b9618e2e05
vutsuak16 <vutsuak96@gmail.com>
2019-04-24 20:19:20 -0700

    boiler plate test code added

--- test/test.py
+++ test/test.py
@@ -4,19 +4,55 @@ import unittest
 #module to be developed further
 
 class TestGUI(unittest.TestCase):
+	
 	def test_gui(self):
 		pass
-        
-        def test_slider(self):
 
-            log = open("logs.log")
+	def test_log(self): 
+		log = open("logs.log")
+		pass
+		
+	def test_eeprom(self): 
+		pass
 
-            print(log.readlines())
+	def test_flash(self): 
+		pass
+	
+	def test_watch(self): 
+		pass
+	
+	def test_tc_0_8bit(self): 
+		pass
+
+	def test_tc_1_8bit(self): 
+		pass
+	
+	def test_16bit(self): 
+		pass
+
+	def test_flash(self): 
+		pass
+	
+	def test_usart_8bit(self): 
+		pass
+	
+	def test_spi(self): 
+		pass
 
 class TestGDB(unittest.TestCase):
-        def test_gdb_connection(self):
-                pass
+	
+	def test_gdb_connection(self):
+		pass
+
+class TestMem(unittest.TestCase):
+	
+	def test_mem(self):
+		pass
+
+	
+
+
 
 
 if __name__ == '__main__':
-	unittest.main()
+        unittest.main()
